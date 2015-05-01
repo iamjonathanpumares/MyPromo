f6c9c83a4a50b5c7cea73bf7d45d50e3b7401149
Jonathan Pumares <jepc1491@gmail.com>
2015-05-01 17:41:52 -0500

    Personalización del admin para la lista de cambios.

--- cupones/admin.py
+++ cupones/admin.py
@@ -1,5 +1,17 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
 from .models import Cupon, UsuariosCupones
 
-admin.site.register(Cupon)
+class CuponAdmin(admin.ModelAdmin):
+	# Especificamos la lista de campos que queremos visualizar en la lista de cambios
+	list_display = ('titulo', 'fecha_inicio', 'vigencia', 'status')
+
+	# Incluimos una barra de búsqueda especificando los campos por los que queremos buscar
+	search_fields = ('titulo',)
+
+	# Se agregan filtros pasando una tupla con los campos con los que queremos filtrar
+	list_filter = ('status', 'vigencia',)
+	date_hierarchy = 'vigencia'
+
+admin.site.register(Cupon, CuponAdmin)
 admin.site.register(UsuariosCupones)
